cc71bcdc1a35f707d33ced759a510c2103af4427
mojichao <jichaomo@gmail.com>
2018-11-20 17:48:07 +0800

    h11

--- src/ios/libs/ZJNative.framework/Headers/ZJNative.h
+++ src/ios/libs/ZJNative.framework/Headers/ZJNative.h
@@ -2,19 +2,12 @@
 //  ZJNative.h
 //  ZJNative
 //
-//  Created by 王吉源 on 2018/9/28.
-//  Copyright © 2018年 juyun. All rights reserved.
+//  Created by mojc on 2018/11/20.
+//
 //
 
 #import <UIKit/UIKit.h>
 
-#import <ZJNative/CloseDelegate.h>
-#import <ZJNative/FirstController.h>
-#import <ZJNative/RootNavigationController.h>
-#import <ZJNative/JudgeCustomerOptional.h>
-#import <ZJNative/Macro.h>
-#import <ZJNative/UIColor+Extension.h>
-#import <ZJNative/CoredataDelegate.h>
 //! Project version number for ZJNative.
 FOUNDATION_EXPORT double ZJNativeVersionNumber;
 
@@ -23,4 +16,10 @@ FOUNDATION_EXPORT const unsigned char ZJNativeVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <ZJNative/PublicHeader.h>
 
-
+#import <ZJNative/CloseDelegate.h>
+#import <ZJNative/FirstController.h>
+#import <ZJNative/RootNavigationController.h>
+#import <ZJNative/JudgeCustomerOptional.h>
+#import <ZJNative/Macro.h>
+#import <ZJNative/UIColor+Extension.h>
+#import <ZJNative/CoredataDelegate.h>
